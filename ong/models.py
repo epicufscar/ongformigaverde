2a2123231a5fac616129f5c4e558cc10954bce52
Renan Peixoto <renanpeixotox@gmail.com>
2017-11-19 16:18:21 -0200

    Added model - Projeto

--- ong/models.py
+++ ong/models.py
@@ -5,6 +5,7 @@ class Membro(models.Model):
     class Meta:
         verbose_name = 'membro'
         verbose_name_plural = 'membros'
+        ordering = ['pais']
 
     def __str__(self):
         return '[' + self.pais + '] ' + self.nome
@@ -13,8 +14,8 @@ class Membro(models.Model):
         self.nome = self.nome.upper()
         self.pais = self.pais.upper()
         self.atividade = self.atividade.upper()
-        self.pais_ingles = self.pais_ingles.upper()
-        self.atividade_ingles = self.atividade_ingles.upper()
+        self.country = self.country.upper()
+        self.activity = self.activity.upper()
         super(Membro, self).save(*args, **kwargs)
 
     nome = models.CharField(blank=False, max_length=100, verbose_name='nome')
@@ -23,10 +24,50 @@ class Membro(models.Model):
     facebook = models.CharField(blank=True, max_length=100, verbose_name='facebook')
     pais = models.CharField(blank=False, max_length=100, verbose_name='país')
     dataInicio = models.DateField(blank=False, verbose_name='data de início das atividades')
-    dataFim = models.DateField(blank=True, null=True, verbose_name='data de término das atividades')
+    dataFim = models.DateField(blank=True, null=True, verbose_name='data de término das atividades, se houver')
     atividade = models.CharField(blank=False, max_length=100, verbose_name='atividade ou função')
     depoimento = models.TextField(blank=True, verbose_name='depoimento')
     # informacoes que precisam ser traduzidas
-    pais_ingles = models.CharField(blank=True, max_length=100, verbose_name='país em inglês')
-    atividade_ingles = models.CharField(blank=True, max_length=100, verbose_name='atividade ou função em inglês')
-    depoimento_ingles = models.TextField(blank=True, verbose_name='depoimento em inglês')
+    country = models.CharField(blank=True, max_length=100, verbose_name='país em inglês')
+    activity = models.CharField(blank=True, max_length=100, verbose_name='atividade ou função em inglês')
+    statement = models.TextField(blank=True, verbose_name='depoimento em inglês')
+
+
+class Projeto(models.Model):
+    class Meta:
+        verbose_name = 'projeto'
+        verbose_name_plural = 'projetos'
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.nome
+
+    def save(self, *args, **kwargs):
+        self.nome = self.nome.upper()
+        self.name = self.name.upper()
+        super(Projeto, self).save(*args, **kwargs)
+
+    PUBLICO = (
+        ('P1', 'CRIANÇAS'),
+        ('P2', 'ADULTOS'),
+        ('P3', 'TODOS')
+    )
+
+    PUBLIC = (
+        ('E1', 'CHILDREN'),
+        ('E2', 'ADULTS'),
+        ('E3', 'EVERYONE')
+    )
+
+    nome = models.CharField(blank=False, max_length=100, verbose_name='nome')
+    descricao = models.TextField(blank=False, verbose_name='descrição')
+    publico = models.CharField(blank=False, max_length=2, choices=PUBLICO, verbose_name='público alvo')
+    dataInicio = models.DateField(blank=False, verbose_name='data de início do projeto')
+    dataFim = models.DateField(blank=True, null=True, verbose_name='data de término do projeto, se houver')
+    linkFotos = models.URLField(blank=True, verbose_name='link para álbum de fotos do projeto')
+    linkVideo = models.URLField(blank=True, verbose_name='link para vídeo do projeto')
+    membros = models.ManyToManyField(Membro, blank=True, verbose_name='membros responsáveis pelo projeto')
+    # informacoes que precisam ser traduzidas
+    name = models.CharField(blank=True, max_length=100, verbose_name='nome em inglês')
+    description = models.TextField(blank=True, verbose_name='descrição em inglês')
+    public = models.CharField(blank=True, max_length=2, choices=PUBLIC, verbose_name='público alvo em inglês')
