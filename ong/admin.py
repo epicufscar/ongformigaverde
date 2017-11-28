2389f0796c5126bff9b4df97d2af10453bfd08d4
Renan Peixoto <renanpeixotox@gmail.com>
2017-11-28 16:59:17 -0200

    Customized admin panel for Noticia, CampanhaParaDoacoes, Parceria, etc.

--- ong/admin.py
+++ ong/admin.py
@@ -45,10 +45,87 @@ class ProjetoAdmin(admin.ModelAdmin):
     )
 
 
+class ParceriaAdmin(admin.ModelAdmin):
+    readonly_fields = ('possuiPhoto',)
+    list_display = ('nome', 'responsavel', 'telefone', 'tipo', 'possuiPhoto',)
+    search_fields = ('nome', 'responsavel', 'tipo', 'endereco')
+    fieldsets = (
+        (None, {
+            'fields': ('nome', 'responsavel', 'telefone', 'endereco', 'link', 'tipo',)
+        }),
+        ('Foto', {
+            'fields': ('photo', 'possuiPhoto', 'apagarPhoto',)
+        }),
+        ('Em Inglês', {
+            'fields': ('type',)
+        })
+    )
+
+
+class CampanhaParaDoacoesAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'projeto', 'dataInicio', 'dataFim',)
+    search_fields = ('titulo',)
+    fieldsets = (
+        (None, {
+            'fields': ('titulo', 'descricao', 'link', 'projeto', 'dataInicio', 'dataFim',)
+        }),
+        ('Em Inglês', {
+            'fields': ('title', 'description')
+        })
+    )
+
+
+class DepoimentoSobreProjetoAdmin(admin.ModelAdmin):
+    readonly_fields = ('possuiPhoto',)
+    list_display = ('nome', 'idade', 'projeto', 'possuiPhoto',)
+    search_fields = ('nome', 'idade',)
+    fieldsets = (
+        (None, {
+            'fields': ('nome', 'idade', 'projeto', 'depoimento',)
+        }),
+        ('Mídias', {
+            'fields': ('photo', 'possuiPhoto', 'apagarPhoto', 'linkVideo',)
+        }),
+        ('Em Inglês', {
+            'fields': ('statement',)
+        })
+    )
+
+
+class NoticiaAdmin(admin.ModelAdmin):
+    readonly_fields = ('possuiPhoto',)
+    list_display = ('titulo', 'data', 'link', 'possuiPhoto')
+    search_fields = ('titulo',)
+    fieldsets = (
+        (None, {
+            'fields': ('data', 'titulo', 'texto',)
+        }),
+        ('Mídias', {
+            'fields': ('photo', 'possuiPhoto', 'apagarPhoto', 'link', 'linkFotos', 'linkVideo',)
+        })
+    )
+
+
+class ReceitaDeDoacoesAdmin(admin.ModelAdmin):
+    list_display = ('data', 'valor', 'anonimo', 'utilizacao', 'meioPagto',)
+    search_fields = ('valor', 'utilizacao', 'meioPagto', 'nome',)
+    fieldsets = (
+        (None, {
+            'fields': ('data', 'valor', 'anonimo', 'nome', 'meioPagto',)
+        }),
+        ('Transparência', {
+            'fields': ('utilizacao', 'comentarios',)
+        }),
+        ('Em Inglês', {
+            'fields': ('payMethod', 'usage',)
+        })
+    )
+
+
 admin.site.register(Membro, MembroAdmin)
 admin.site.register(Projeto, ProjetoAdmin)
-admin.site.register(CampanhaParaDoacoes)
-admin.site.register(DepoimentoSobreProjeto)
-admin.site.register(Parceria)
-admin.site.register(ReceitaDeDoacoes)
-admin.site.register(Noticia)
+admin.site.register(CampanhaParaDoacoes, CampanhaParaDoacoesAdmin)
+admin.site.register(DepoimentoSobreProjeto, DepoimentoSobreProjetoAdmin)
+admin.site.register(Parceria, ParceriaAdmin)
+admin.site.register(ReceitaDeDoacoes, ReceitaDeDoacoesAdmin)
+admin.site.register(Noticia, NoticiaAdmin)
