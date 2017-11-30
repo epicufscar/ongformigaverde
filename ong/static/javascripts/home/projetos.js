5ea9dbf5420982c99c5ad526b5427cfb4027d560
Renan Peixoto <renanpeixotox@gmail.com>
2017-11-30 01:39:37 -0200

    Moved script to appropriate file

--- ong/static/javascripts/home/projetos.js
+++ ong/static/javascripts/home/projetos.js
@@ -1,16 +1,20 @@
-jQuery(document).ready(function($) {
-
-	var owl = $("#owl-demo-2");
-  owl.owlCarousel({
-      items : 3, 
-      itemsDesktop : [992,3],
-      itemsDesktopSmall : [768,2], 
-      itemsTablet: [480,2], 
-      itemsMobile : [320,1]
-  });
-  $(".next").click(function(){ owl.trigger('owl.next'); });
-  $(".prev").click(function(){ owl.trigger('owl.prev'); });
-
-$('.latest-blog-posts .thumbnail.item').matchHeight();
-	
+$('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        },
+        1200: {
+            items: 4
+        }
+    }
 });
