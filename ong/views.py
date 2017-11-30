16188932e4924d807c41168f76cbe95a04ba5f8e
Juliana Karoline <julianaklulo@gmail.com>
2017-11-30 11:48:56 -0200

    Updated _em_numeros to display fetched data from the database

--- ong/views.py
+++ ong/views.py
@@ -4,7 +4,10 @@ from .models import *
 
 def home(request):
     data = {
-        'informacoes_ong': InformacoesGeraisONG.objects.first()
+        'informacoes_ong': InformacoesGeraisONG.objects.first(),
+        'projetos_andamento': Projeto.objects.filter(dataFim=None).count(),
+        'parceiros': Parceria.objects.count(),
+        'intercambistas': Membro.objects.exclude(pais='BRASIL').count()
     }
     return render(request, 'ong/home/home.html', data)
 
