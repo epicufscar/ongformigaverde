b1b63dd37680d1571aafbf4b507ce1352a8bf5c4
Juliana Karoline <julianaklulo@gmail.com>
2017-12-09 15:10:26 -0200

    Added views to load remaining pages

--- ong/views.py
+++ ong/views.py
@@ -11,3 +11,42 @@ def home(request):
     }
     return render(request, 'ong/home/home.html', data)
 
+
+def historia(request):
+    data = {}
+    return render(request, 'ong/historia.html', data)
+
+
+def missao(request):
+    data = {}
+    return render(request, 'ong/missao.html', data)
+
+
+def equipe(request):
+    data = {}
+    return render(request, 'ong/equipe.html', data)
+
+
+def parceiros(request):
+    data = {}
+    return render(request, 'ong/parceiros.html', data)
+
+
+def projetos(request):
+    data = {}
+    return render(request, 'ong/projetos.html', data)
+
+
+def doacoes(request):
+    data = {}
+    return render(request, 'ong/doacoes.html', data)
+
+
+def transparencia(request):
+    data = {}
+    return render(request, 'ong/transparencia.html', data)
+
+
+def noticias(request):
+    data = {}
+    return render(request, 'ong/noticias.html', data)
